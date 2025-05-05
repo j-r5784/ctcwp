429b3fb2c84b769d8ce3fdd08bc7dc4b6220c7f5
Jran99 <133728234+j-r5784@users.noreply.github.com>
2025-05-05 13:10:19 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function goToScratch(){
+    window.open("https://scratch.mit.edu/users/doomgoblin17", '_blank').focus();
+}
+
+function goToGitHub(){
+    window.open("https://github.com/j-r5784", '_blank').focus();
+}
